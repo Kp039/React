7e0d3335addbe624bceb1116a34b76e7811ba8c0
Kp <v31mn@pn.me>
2026-07-08 13:50:16 +0530

    Name change

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const handeleNameChange = () => {
+    const names = ["Bob", "Kevin", "Dave", "Bhoomi"];
+    const int = Math.floor(Math.random() * 4);
+    return names[int];
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to changes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {handeleNameChange()}!</p>
       </header>
     </div>
   );
