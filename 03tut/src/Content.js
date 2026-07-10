35310d7806f0461b1ba9965ee800b523fd261502
Kp <v13mn@pm.me>
2026-07-10 19:17:52 +0530

    Click Events

--- 03tut/src/Content.js
+++ 03tut/src/Content.js
@@ -4,10 +4,26 @@ const Content = () => {
     const int = Math.floor(Math.random() * 4);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log("You clicked it")
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} clicked it`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
   
   return (
     <main>
-      <p> Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}> Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click IT</button>
+      <button onClick={() => handleClick2("Dave")}>Click IT</button>
+      <button onClick={(e) => handleClick3(e)}>Click IT</button>
+      
     </main>
   );
 };
