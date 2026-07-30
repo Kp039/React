b74d8c77030d9b9fd566084fb6e2de5987dbee88
Kp <v13mn@pm.me>
2026-07-30 17:03:21 +0530

    Project Challenge

---
+++ 10tut/src/Square.js
@@ -0,0 +1,13 @@
+const Square = ({ colorValue, hexValue, isDarkText }) => {
+  return (
+    <section
+      className="square"
+      style={{ background: colorValue, color: isDarkText ? "#000" : "#FFF" }}
+    >
+      <p>{colorValue ? colorValue : "Empty Value"}</p>
+      <p>{hexValue ? hexValue : null}</p>
+    </section>
+  );
+};
+
+export default Square;
