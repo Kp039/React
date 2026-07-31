709e543be1f70fa406b26df8e34b045b3fd1f82f
Kp <v13mn@pm.me>
2026-07-31 20:15:46 +0530

    useEffect Hook

---
+++ 11tut/src/Content.js
@@ -0,0 +1,19 @@
+import ItemList from "./ItemList";
+
+const Content = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <main>
+      {item.length ? (
+        <ItemList
+          item={item}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
+      )}
+    </main>
+  );
+};
+
+export default Content;
